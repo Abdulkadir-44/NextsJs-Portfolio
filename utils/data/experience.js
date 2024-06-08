2683ddf51862e7896b8e4b8a47a8a57489cf82ef
Abdulkadir Özenç <abdulkadirozenc192@gmail.com>
2024-06-08 21:36:01 +0300

    data değişiklikleri

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,8 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Frontend Developer ',
+    title: 'Frontend and Backend Developer ',
     company: "Institute of Project Support and Solidarity",
-    duration: "(Jan 2022 - Present)"
+    duration: "(September 15 , 2023 - April 1 , 2024)"
+  },
+  {
+    id: 2,
+    title: 'Frontend Developer ',
+    company: "Schio dış ticaret Limited Şirketi",
+    duration: "(May 2 , 2024 - Present)"
   }
 ]
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,12 +2,12 @@ export const personalData = {
   name: "Abdulkadir Özenç",
   profile: '/profile.png',
   designation: "Frontend Developer",
-  description: " I'm Abdulkadir Özenç. As a third-year student, I stand out with my passion for the field of software development, particularly my interest in modern technologies like React.js. Although I'm at the beginning of my career journey, I strive to continuously enhance my knowledge and experience.I'mcurrently pursuing my education at Iskenderun Technical University ,  majoring in Computer Science. I focus on improving my skills in software development by reinforcing the theoretical knowledge I acquire in my classes with practical projects and personal studies.My keen interest in modern web technologies,especially React.js",
+  description: " I'm Abdulkadir Özenç. I stand out with my passion for software development, especially my interest in modern technologies like React.js. I've been working on web development for about 2 years. Currently,I'm pursuing my education in Computer Science at İskenderun Technical University, reinforcing theoretical knowledge with practical projects. I develop user-friendly and interactive web applications using technologies such as TailwindCSS, React, MongoDB ,JavaScript, Express.js, Node.js, ShadCN,and C#. Collaboration and continuous learning are essential for me.",
   email: 'abdulkadirozenc5371@gmail.com',
   phone: '0555 555 55 55',
   address: 'Osmaniye / Turkey ',
   github: 'https://github.com/Abdulkadir-44',
   linkedIn: 'https://www.linkedin.com/in/abdulkadirozenc/',
   stackOverflow: 'https://stackoverflow.com/users/23891573/abdulkadir?tab=profile',
-  resume: "https://drive.google.com/file/d/1Z4Dld8NNkP5KNl0W_zqluahXqB8MolH6/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/106hnh03Vw_Eq4tHRY76lmGUklcqoUjK4/view?usp=sharing"
 }
