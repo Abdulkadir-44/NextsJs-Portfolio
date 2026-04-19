51a653dfb7aaace03cebf6dda3c34d84c35442d6
Abdulkadir Özenç <116664446+Abdulkadir-44@users.noreply.github.com>
2026-04-19 22:46:01 +0300

    Refactor skill image imports to use direct paths

--- utils/skill-image.js
+++ utils/skill-image.js
@@ -1,84 +1,46 @@
-  
-import aws from '/public/svg/skills/aws.svg';
-
-import bootstrap from '/public/svg/skills/bootstrap.svg';
-
-import c from '/public/svg/skills/c.svg';
-import canva from '/public/svg/skills/canva.svg';
-
-import cplusplus from '/public/svg/skills/cplusplus.svg';
-import csharp from '/public/svg/skills/csharp.svg';
-import css from '/public/svg/skills/css.svg';
-
-import docker from '/public/svg/skills/docker.svg';
-
-import firebase from '/public/svg/skills/firebase.svg';
-
-import git from '/public/svg/skills/git.svg';
-
-import html from '/public/svg/skills/html.svg';
-import javascript from '/public/svg/skills/javascript.svg';
-import materialui from '/public/svg/skills/materialui.svg';
-import mongoDB from '/public/svg/skills/mongoDB.svg';
-import mysql from '/public/svg/skills/mysql.svg';
-import react from '/public/svg/skills/react.svg';
-import tailwind from '/public/svg/skills/tailwind.svg';
-import vitejs from '/public/svg/skills/vitejs.svg';
-
-
-
 export const skillsImage = (skill) => {
   const skillID = skill.toLowerCase();
+  
+
+  
   switch (skillID) {
     case 'html':
-      return html;
+      return '/svg/skills/html.svg';
     case 'docker':
-      return docker;
+      return '/svg/skills/docker.svg';
     case 'css':
-      return css;
-
+      return '/svg/skills/css.svg';
     case 'javascript':
-      return javascript;
-
+      return '/svg/skills/javascript.svg';
     case 'react':
-      return react;
-
+      return '/svg/skills/react.svg';
     case 'bootstrap':
-      return bootstrap;
-
+      return '/svg/skills/bootstrap.svg';
     case 'mongodb':
-      return mongoDB;
+      return '/svg/skills/mongoDB.svg';
     case 'mysql':
-      return mysql;
-
+      return '/svg/skills/mysql.svg';
     case 'tailwind':
-      return tailwind;
+      return '/svg/skills/tailwind.svg';
     case 'vitejs':
-      return vitejs;
-
+      return '/svg/skills/vitejs.svg';
     case 'c':
-      return c;
+      return '/svg/skills/c.svg';
     case 'c++':
-      return cplusplus;
+      return '/svg/skills/cplusplus.svg';
     case 'c#':
-      return csharp;
-
-
+      return '/svg/skills/csharp.svg';
     case 'aws':
-      return aws;
-
+      return '/svg/skills/aws.svg';
     case 'firebase':
-      return firebase;
-
+      return '/svg/skills/firebase.svg';
     case 'git':
-      return git;
-
+      return '/svg/skills/git.svg';
     case 'materialui':
-      return materialui;
-
+      return '/svg/skills/materialui.svg';
     case 'canva':
-      return canva;
+      return '/svg/skills/canva.svg';
     default:
-      break;
+      return null; 
   }
 }
